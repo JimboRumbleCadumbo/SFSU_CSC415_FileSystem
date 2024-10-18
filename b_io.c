e6c1c5dabd024f9a8b702c0b5fe05e76ee02f9f2
Rishita317 <rmeharishi@mail.sfsu.edu>
2024-10-18 13:47:17 -0700

    File description updated

--- b_io.c
+++ b_io.c
@@ -1,9 +1,9 @@
 /**************************************************************
-* Class::  CSC-415-0# Spring 2024
-* Name::
-* Student IDs::
-* GitHub-Name::
-* Group-Name::
+* Class::  CSC-415-03 Fall 2024
+* Name:: Yu-Ming Chen, Ria Thakker, Yuquan Xu, Rishita Meharishi 	
+* Student IDs:: 923313947, 922593253, 920390312, 922249026
+* GitHub-Name:: JimboRumbleCadumbo, riathakker, yyyuquan, Rishita317 
+* Group-Name:: Bytes Busters
 * Project:: Basic File System
 *
 * File:: b_io.c
