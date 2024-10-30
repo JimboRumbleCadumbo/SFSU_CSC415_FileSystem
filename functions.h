5a14a54f37e6ad7b583a9a3b5dbff39a9c9ee951
JimboRumbleCadumbo <1297275552jimjim@gmail.com>
2024-10-29 17:03:21 -0700

    Added functions.h

---
+++ functions.h
@@ -0,0 +1,29 @@
+/**************************************************************
+* Class::  CSC-415-03 Fall 2024
+* Name:: Yu-Ming Chen, Ria Thakker, Yuquan Xu, Rishita Meharishi 	
+* Student IDs:: 923313947, 922593253, 920390312, 922249026
+* GitHub-Name:: JimboRumbleCadumbo, riathakker, yyyuquan, Rishita317 
+* Group-Name:: Bytes Busters
+* Project:: Basic File System
+*
+* File:: functions.h
+*
+* Description:: A combination of all the functions in fsReadWrite,
+* fsFreeSpace, fsInitDir.c
+*
+**************************************************************/
+
+#include "fsReadWrite.c"
+#include "fsFreeSpace.c"
+#include "fsFreeSpaceAllo.c"
+#include "fsInitDir.c"
+#include "structs.h"
+
+int allocateBlocks(int numBlocks);
+int releaseBlocks(int numToRelease, int startingBlock);
+int discontinuousWrite(int startingBlock, void *buffer);
+int discontinuousRead(int startingBlock, void *buffer);
+int initializeFAT();
+DE * createDirectory(int numEntries, DE *parent);
+int writeDir(DE *dir);
+
