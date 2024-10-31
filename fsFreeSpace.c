460e2a2f04cc95dd060958270edb8aa4fbe8e4eb
student <riathakker14@gmail.com>
2024-10-30 18:38:35 -0700

    Fix overallocation of blocks

--- fsFreeSpace.c
+++ fsFreeSpace.c
@@ -23,12 +23,13 @@
 int initializeFAT(int blockSize, int numBlocks) {
     // Allocate memory for the FAT
     int totalBlocks = ((numBlocks * sizeof(int)) + (blockSize - 1))/blockSize;
-    totalBlocks++;
+    totalBlocks;
     int totalBytes = totalBlocks * blockSize;
     fat = (int *)malloc(totalBytes);
     if (fat == NULL) {
         return -1; // Memory allocation failed
     }
+    printf("Total blocks: %d\n", totalBlocks);
 
 //  // link everything together 0 by itself and then block 1-2, 3-4 etc 
 // and thats how the total is FAT i is i+1 
@@ -38,15 +39,15 @@ int initializeFAT(int blockSize, int numBlocks) {
     for (int i = 0; i < numBlocks; i++) {
         if (i == 0) {
             fat[i] = -1; // VCB is by itself
-        } else if (i < totalBlocks) {
-            fat[i] = i + 1; // Link the blocks together
-        } else {
-            fat[i] = i + 1; // Mark the rest as free
+        } else if (i == totalBlocks || i == numBlocks - 1) {
+            // Mark the end of chain for the FAT table and the free space
+            fat[i] = END_OF_CHAIN; 
+        }
+        else {
+            fat[i] = i + 1; // Link everything else together.
         }
     }
-    fat[totalBlocks] = END_OF_CHAIN; // Mark the last block (156) as the end
-    fat[numBlocks-1] = END_OF_CHAIN;
-    vcb->freeSpaceLoc = fat[totalBlocks + 1];
+    vcb->freeSpaceLoc = totalBlocks + 1;
     
     // Write the FAT to disk
     if (discontinuousWrite(1, fat) != totalBlocks) {
