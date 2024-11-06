f835e936a0007f6ce569c94129a9c32e52141850
student <yxu15@mail.sfsu.edu>
2024-11-05 23:22:11 -0800

    Added functions isFile, isDir, and fsStat, will need to be updated

--- mfs.c
+++ mfs.c
@@ -42,14 +42,50 @@ char * fs_getcwd(char *pathname, size_t size){
 
 int fs_setcwd(char *pathname){ //linux chdir
     return 0;
-}   
+}
+
 int fs_isFile(char * filename){ //return 1 if file, 0 otherwise
-    return 0;
+    struct fs_diriteminfo dirEntry;
+
+    if (fetchDirEntry(filename, &dirEntry) != 0) {
+        return 0;
+    }
+    // Not ready yet
+    // return dirEntry.fileType == ...;
 }	
 
 int fs_isDir(char * pathname){ //return 1 if directory, 0 otherwise
+    struct fs_diriteminfo dirEntry;
+
+    if (fetchDirEntry(pathname, &dirEntry) != 0) {
+        return 0;
+    }
+    // Not ready yet
+    // return dirEntry.fileType == ...;
+}
+
+// Function to retrieve file or directory data
+int fs_stat(const char *path, struct fs_stat *buf) {
+    struct fs_diriteminfo dirEntry;
+
+    if (fetchDirEntry(path, &dirEntry) != 0) {
+        return -1; // Invalid path
+    }
+
+    memset(buf, 0, sizeof(struct fs_stat));  // Clear the struct
+    buf->st_size = dirEntry.d_reclen;  // Assumed the size matches d_reclen
+    buf->st_blksize = 512;
+    buf->st_blocks = (buf->st_size + 511) / 512;
+
+    buf->st_accesstime = time(NULL);  
+    buf->st_modtime = time(NULL);
+    buf->st_createtime = time(NULL);
+
+    // buf->st_mode = (dirEntry.fileType == ...);
+
     return 0;
-}		
+}
+
 int fs_delete(char* filename){ //removes a file
     return 0;
 }	
